06259949b9fd710dc6b6114ab8d601ba7565d9d6
Boy Thanapong <thanapongsj1996@gmail.com>
2023-11-08 23:01:05 +0800

    init project

---
+++ db/postgres.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+
+// postgres
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+  }
+);
+
+module.exports = sequelize;
